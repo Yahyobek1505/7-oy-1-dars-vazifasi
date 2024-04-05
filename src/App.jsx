fc82bbaa4e5be8d283c4c12c180d714cdbcbc6b4
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-04-06 02:18:54 +0500

    changed!

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>Hello  world</h1>
+      <h1 className='text-bold w-[45rem] text-justify mx-auto text-4xl'>Imtihon va darslar judayam bir biriga yaqin bo'lib qolgani uchun bu vazifani ham aytilgan vaqtida to'liq bajara 
+         olmadim, buning uchun <span className='uppercase text-red-500 font-semibold'>uzr so'rayman</span>.
+         Lekin, Hudo xohlasa <span className='uppercase text-red-500 font-regular text-3xl'>06.04.2024 kuni soat kechki 11:00</span> gacha albatta to'liq holatda ERP ga yuklayman! <br /><br /> Current time - 02:18
+      </h1>
     </div>
   )
 }
